cf9d77f84997ef5261b5fd175a18ca9003594503
cedricDev97 <somcedric002@gmail.com>
2022-03-13 10:01:25 -0700

    commit task 2,4,5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * function main who start the program
+ * printf function who display the content of programm
+ * function Return who return 0 or 1
+ **/
+ int main (void)
+    {
+    	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+    
+    }
