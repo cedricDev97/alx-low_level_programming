4e91d8d1fb9243de0eefd075de8cd7945e68821a
cedricDev97 <somcedric002@gmail.com>
2022-03-17 13:46:51 -0700

    commit task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,19 +4,19 @@
  *
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 int i;
 
 printf("Infinite loop incoming :(\n");
 
-i = 0
+i = 0;
 
 /*
  *while (i < 10)
  *{
  *Infinite loop - No increase of variable
- * putchar(i);
+ *putchar(i);
  }
 */
 printf("Infinite loop avoided! \\o/\n");
