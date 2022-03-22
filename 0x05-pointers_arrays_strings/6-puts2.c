5bbb55ab255bc6d65afac622b7c21f945cbebe87
cedricDev97 <somcedric002@gmail.com>
2022-03-21 17:07:15 -0700

    my commit task pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts2 - print very other characters in string.
+ * @str: pointer to string.
+ *
+ * Return: void.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+		
+			_putchar(str[i]);
+		
+		}
+		i++;
+
+	
+	}
+	_putchar('\n');
+
+}
