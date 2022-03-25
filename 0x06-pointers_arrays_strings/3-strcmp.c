ed10fb44b4a3778c43e55414fe325cc39b6ba0be
cedricDev97 <somcedric002@gmail.com>
2022-03-25 05:13:21 -0700

    commit message task 4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,12 @@
-#include "main.h"
+include "main.h"
 /**
  * _strcmp - compares two strings
  * @s1: A pointer to a character that will be changed
- * @s2: A pointer to a character that will also be changed/modified/update
+ * @s2: A pointer to a character that will also be changed/modified/updated
  * Return: dest
  */
 
 int _strcmp(char *s1, char *s2)
-
 {
 
 char *str_one = s1;
