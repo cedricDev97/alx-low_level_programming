dab0f416bd3d139dc4361684ea89ba17b537a82a
cedricDev97 <somcedric002@gmail.com>
2022-03-25 20:02:24 -0700

    task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,25 +8,24 @@
 
 char *cap_string(char *s)
 {
-int count = 0;
-while (s[count] != '\0')
+int k, l;
+
+char sep[13] = {' ', '\t', '\n', ',', ':', '.', '!', '?', '"', '(', ')', '{', '}'};
+for (k = 0; s[k] != '\0'; k++)
 {
-if (s[0] >= 97 && s[0] <= 122)
+if (k == 0 && s[k] >= 'a' && s[k] <= 'z')
+s[k] -= 32;
+
+for (l = 0; l < 13; l++)
 {
-s[0] = s[0] - 32;
+if (s[k] == sep[l])
+{
+if (s[]k + 1 >= 'a' && s[k + 1] <= 'z')
+{
+s[k + 1] -= 32;
 }
-if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-	|| s[count] == ',' || s[count] == ';' || s[count] == '.'
-	|| s[count] == '.' || s[count] == '!' || s[count] == '?'
-	|| s[count] == '"' || s[count] == '(' || s[count] == ')'
-	|| s[count] == '{' || s[count] '}')
-{
-	if (s[count + 1] >= 97 && s[count + 1] <= 122)
-	{
-		s[count + 1] = s[count + 1] - 32;	
-	}
 }
-count++;
+}
 }
 return (s);
 }
