4e432eb9f2ce8c7cc500e45c23103c50e9e2dd59
cedricDev97 <somcedric002@gmail.com>
2022-04-08 16:17:23 -0700

    update task more malloc free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _calloc -allocated memoria for the size bytes
+ * _calloc - allocated memoria for nmemb the size bytes
  * @nmemb: number of element in the array
  * @size: bytes for each position in array
  * Return: pointer void
@@ -12,16 +12,20 @@ char *p;
 unsigned int i;
 
 if (nmemb == 0 || size == 0)
+{
 return (NULL);
+}
 
 p = malloc(nmemb * size);
 if (p == NULL)
+{
 return (NULL);
-
+}
 for (i = 0; i < nmemb * size; i++)
+{
 P[i] = 0;
+}
 
 return (p);
-
 
 }
