2823d8a947935b0ef15420136cecbae799b34019
cedricDev97 <somcedric002@gmail.com>
2022-06-12 00:41:57 +0000

    update

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,8 +7,10 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *current;
 	dlistint_t *next;
+
 	current = head;
-	while(current != NULL)
+
+	while (current != NULL)
 	{
 		next = current->next;
 		free(current);
